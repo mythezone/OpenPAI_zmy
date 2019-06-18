10b39fd18aead7d66b796c317b610adfee625a3a
mythezone <125616515@qq.com>
2019-06-18 22:18:50 +0800

    Update tsp_test.py

--- TSP_on_PAI/tsp_test.py
+++ TSP_on_PAI/tsp_test.py
@@ -1,7 +1,7 @@
 import sys
 from random import random
 from operator import add
-
+#import numpy as np
 from pyspark.sql import SparkSession
 
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         .builder\
         .appName("PythonPi")\
         .getOrCreate()
-    import create
+    #import create
     partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
     n = 100000 * partitions
 
