2b0c703b5cf8e427860465243452d49c3720adf5
mythezone <125616515@qq.com>
2019-06-18 20:55:42 +0800

    for testing

--- TSP_on_PAI/tsp_loop.py
+++ TSP_on_PAI/tsp_loop.py
@@ -3,7 +3,7 @@ import os_file as to
 import iteration as it
 import numpy as np
 import time,os,sys
-from pai_pyhdfs import *
+#from pai_pyhdfs import *
 from pyspark.context import SparkContext
 
 #-------------initiation-------------#
@@ -14,8 +14,8 @@ max_iter=10000
 threshold=10.0
 work_path='/shared/TSP/'
 print("waiting for the matrix.")
-f=wait_hdfs_file(work_path,'distance_matrix.npy',delete=False)
-m=np.load(f,allow_pickle=True) #save distance_matrix in m.
+#f=wait_hdfs_file(work_path,'distance_matrix.npy',delete=False)
+#m=np.load(f,allow_pickle=True) #save distance_matrix in m.
 
 def get_res(s1,s2):
     if tc.cost(s1,m)<tc.cost(s2,m):
@@ -25,17 +25,18 @@ def get_res(s1,s2):
 
 #------------initiation over---------#
 
-while True:
-    f=wait_hdfs_file(work_path,'generations.npy',delete=False)
-    generations=np.load(f)
-    print("generation information getted. Now setting the gen_rdd.")
-    gen_rdd=sc.parallelize(generations)
-    print("rdd setted,now maping.")
-    res=gen_rdd.map(lambda x:it.iteration(x,m))
-    print("mapping over,now reducing.")
-    res2=res.reduce(get_res)
-    print("The best solution is:",res2)
-    c=tc.cost(res2,m)
-    print("The lowest cost is :",c)
-    hdfs_save(work_path,'final_solution.npy',[res2,c])
+print("GOOD")
+# while True:
+#     f=wait_hdfs_file(work_path,'generations.npy',delete=False)
+#     generations=np.load(f)
+#     print("generation information getted. Now setting the gen_rdd.")
+#     gen_rdd=sc.parallelize(generations)
+#     print("rdd setted,now maping.")
+#     res=gen_rdd.map(lambda x:it.iteration(x,m))
+#     print("mapping over,now reducing.")
+#     res2=res.reduce(get_res)
+#     print("The best solution is:",res2)
+#     c=tc.cost(res2,m)
+#     print("The lowest cost is :",c)
+#     hdfs_save(work_path,'final_solution.npy',[res2,c])
 
