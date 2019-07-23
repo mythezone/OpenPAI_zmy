e8351374c225b9047d14e8a08b127d8b87caa311
IdiosyncraticDragon <lgy807720302@gmail.com>
2019-07-23 13:23:25 +0800

    debug

--- TSP_local/additional_args.py
+++ TSP_local/additional_args.py
@@ -1,6 +1,8 @@
 import argparse
+import sys
 
 def get_args():
+   print(sys.argv)
    parse = argparse.ArgumentParser(description="arguments for the demo")
    parse.add_argument('--master_ip_list', required=True, type=str)
    parse.add_argument('--master_port_list', required=True, type=str)
