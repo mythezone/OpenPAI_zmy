245f8bb2a12b209e3ad51ea91b2ab4278287ae7c
IdiosyncraticDragon <lgy807720302@gmail.com>
2019-07-23 19:48:39 +0800

    debug

--- TSP_local/log.py
+++ TSP_local/log.py
@@ -2,10 +2,10 @@ def generate_log_func(log_type, file_name):
    LOG_TYPE=log_type # only 'file' and 'print' reserved
    FILE_NAME = file_name
 
-   def log_info(info_str):
+   def log_info(*info_str):
         if LOG_TYPE == 'file':
           with open(FILE_NAME, 'a+') as fi:
-             fi.write(info_str)
+             fi.write(str(info_str))
              fi.write('\n')
         elif LOG_TYPE == 'print':
           print(info_str)
